dbf885ee1cdec77c922e423a6af685bf4a90b14f
Anderson Lomba <andersonlomba172@outlook.com>
2022-09-07 14:43:24 -0300

    Add project files.

---
+++ ImpostosDesignPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ImpostosDesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCalculoICMS(Orcamento orcamento)
+        {
+            
+        }
+    }
+}
---
+++ ImpostosDesignPatterns/ICMS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostosDesignPatterns
+{
+    public class ICMS
+    {
+        public double CalculaICMS(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.01;
+        }
+    }
+}
